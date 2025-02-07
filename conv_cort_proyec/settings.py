81113c7c1cb705a2c3f94dbc30db5fbcc401c3dd
mlopez8621 <leonardo.lopez86a@gmail.com>
2025-02-07 09:33:44 -0500

    Corrigiendo error de dj_database_url

--- conv_cort_proyec/settings.py
+++ conv_cort_proyec/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-10t(t(6oy^@)yigt49yk!t8(c*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
