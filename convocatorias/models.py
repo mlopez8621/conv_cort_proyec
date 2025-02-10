ed3df7ff6de87d3da01ad91df0759099b29e5f2e
mlopez8621 <leonardo.lopez86a@gmail.com>
2025-02-10 11:32:55 -0500

    Arreglar tipos de datos

--- convocatorias/models.py
+++ convocatorias/models.py
@@ -25,10 +25,25 @@ class Postulacion(models.Model):
         ('otro', 'Otro'),
     ]
 
+    POSTULADO_ANTES_CHOICES = [
+        ('si', 'Sí'),
+        ('no', 'No'),
+    ]
+
+    ACEPTA_TYC_CHOICES= [
+        ('si', 'Sí'),
+        ('no', 'No'),
+    ]
+
+    TIPO_PERSONA_CHOICES = [
+        ('natural', 'Persona Natural'),
+        ('juridica', 'Persona Jurídica'),
+    ]
+
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con el usuario que postula
     correo = models.TextField()
     titulo = models.CharField(max_length=255)
-    anio_produccion = models.CharField(max_length=255)
+    anio_produccion = models.IntegerField()
     duracion = models.TextField()
     formato_grabacion = models.TextField()
     productor_emp_produc = models.TextField()
@@ -45,6 +60,18 @@ class Postulacion(models.Model):
     fecha_resolucion_cpn = models.TextField() 
     certificacion_cpn = models.FileField(upload_to='certificaciones/', null=True, blank=True)
     acta_clasificacion = models.FileField(upload_to='certificaciones/', null=True, blank=True)
+    tipo_persona = models.CharField(max_length=10, choices=TIPO_PERSONA_CHOICES,default='natural')
+    autorizacion_uso = models.FileField(upload_to='autorizaciones/', null=True, blank=True)
+    enlace_vimeo = models.URLField(max_length=500, verbose_name="Enlace del corto en Vimeo", help_text="Debe ser un enlace válido de Vimeo.")
+    contrasena_vimeo = models.CharField(max_length=50, verbose_name="Contraseña del corto en Vimeo", blank=True, null=True)
+    principales_festivales = models.TextField()
+    nombre_productor = models.TextField()
+    celular_productor = models.TextField()
+    domicilio_productor = models.TextField()
+    correo_productor = models.TextField()
+    postulado_antes = models.CharField(max_length=2, choices=POSTULADO_ANTES_CHOICES, default='no')
+    certificacion_cumplimiento = models.FileField(upload_to='certificaciones/', null=True, blank=True)
+    acepta_tyc = models.CharField(max_length=2, choices=ACEPTA_TYC_CHOICES, default='no')
     fecha_postulacion = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='pendiente')
 
