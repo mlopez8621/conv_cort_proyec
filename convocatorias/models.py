40eaa7d6d7a36fc11e86d45831221cee0b672671
mlopez8621 <leonardo.lopez86a@gmail.com>
2025-02-10 21:35:03 -0500

    Formato de campos

--- convocatorias/models.py
+++ convocatorias/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User  # Usamos el modelo predeterminado de Django
+from django.core.validators import RegexValidator
 
 class Postulacion(models.Model):
     ESTADO_CHOICES = [
@@ -41,21 +42,21 @@ class Postulacion(models.Model):
     ]
 
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con el usuario que postula
-    correo = models.TextField()
+    correo = models.EmailField(unique=True, max_length=255)
     titulo = models.CharField(max_length=255)
-    anio_produccion = models.IntegerField()
+    anio_produccion = models.IntegerField(verbose_name="Año de producción")
     duracion = models.TimeField(verbose_name="Duración (Min)", help_text="Formato HH:MM")
-    formato_grabacion = models.TextField()
-    productor_emp_produc = models.TextField()
-    nom_director = models.TextField()
-    sinopsis_corta = models.TextField()
+    formato_grabacion = models.CharField(max_length=255)
+    productor_emp_produc = models.CharField(max_length=255,verbose_name="Productor o empresa productora")
+    nom_director = models.CharField(max_length=255,verbose_name="Nombre del director")
+    sinopsis_corta = models.TextField(verbose_name="Storyline / Sinopsis Corta")
     locaciones_rodaje = models.TextField()
-    beneficiario_fdc = models.CharField(max_length=2, choices=BENEFICIARIO_CHOICES, default='no')
-    anio_fdc = models.PositiveIntegerField(null=True, blank=True)
-    certificacion_fdc = models.FileField(upload_to='certificaciones/', null=True, blank=True)
+    beneficiario_fdc = models.CharField(max_length=2, choices=BENEFICIARIO_CHOICES, default='no',verbose_name="El corto ha sido beneficiario de la convocatoria del FDC para cortos")
+    anio_fdc = models.PositiveIntegerField(null=True, blank=True,verbose_name="Año del beneficio")
+    certificacion_fdc = models.FileField(upload_to='certificaciones/', null=True, blank=True,verbose_name="Certificado del beneficio FDC")
     exhibicion_salas = models.CharField(max_length=2,choices=EXHIBICION_CHOICES,default='no')
     plataformas_exhibicion = models.CharField(max_length=10, choices=PLATAFORMAS_CHOICES,default='no')
-    si_plataforma = models.CharField(max_length=255, blank=True, null=True)
+    si_plataforma = models.CharField(max_length=255, blank=True, null=True,verbose_name="¿Cuales plataformas?")
     resolucion_cpn = models.PositiveIntegerField(null=True, blank=True)
     fecha_resolucion_cpn = models.DateField(null=True, blank=True)
     certificacion_cpn = models.FileField(upload_to='certificaciones/', null=True, blank=True)
@@ -65,10 +66,10 @@ class Postulacion(models.Model):
     enlace_vimeo = models.URLField(max_length=500, verbose_name="Enlace del corto en Vimeo", blank=True, null=True)
     contrasena_vimeo = models.CharField(max_length=50, verbose_name="Contraseña del corto en Vimeo", blank=True, null=True)
     principales_festivales = models.TextField()
-    nombre_productor = models.TextField()
-    celular_productor = models.TextField()
-    domicilio_productor = models.TextField()
-    correo_productor = models.TextField()
+    nombre_productor = models.CharField(max_length=255)
+    celular_productor = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+?\d{1,15}$',            message="Número inválido. Use el formato: +573001234567 o 3001234567")],verbose_name="Número de celular del productor")
+    domicilio_productor = models.CharField(max_length=255)
+    correo_productor = models.EmailField(unique=True, max_length=255)
     postulado_antes = models.CharField(max_length=2, choices=POSTULADO_ANTES_CHOICES, default='no')
     certificacion_cumplimiento = models.FileField(upload_to='certificaciones/', null=True, blank=True)
     acepta_tyc = models.CharField(max_length=2, choices=ACEPTA_TYC_CHOICES, default='no')
